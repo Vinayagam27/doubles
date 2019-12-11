cc95e1ee8eea5f58c5ed77658ba5fc06377d7777
unknown <vinayagamsankar@gmail.com>
2019-12-11 21:52:12 +0530

    Initial commit

---
+++ src/doubles.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+import java.util.Scanner;
+public class doubles {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        double t = Double.parseDouble(in.nextLine());
+        System.out.println(Math.round(t));
+
+    }
+}
